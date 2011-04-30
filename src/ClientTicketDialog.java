5cf4072cfaa7c5bc18164d4646aff8545afcc741
Eric So <services@ericso.me>
2011-04-30 12:10:12 -0400

    Update on Saturday, April 30, afternoon

--- src/ClientTicketDialog.java
+++ src/ClientTicketDialog.java
@@ -20,10 +20,18 @@ public class ClientTicketDialog extends JDialog {
 	private String ticketID;
 	private HashMap<String, Ticket> activeTickets;
 	
-	private JTextField textField;
-	private JTextArea textArea;
+	private JLabel ticketIDLabel;
 	
-	private SendMessageHandler sendMessageHandler;
+	private JTextArea summaryDescriptionTextArea;
+	private JTextArea resolutionDescriptionTextArea;
+	
+	private JTextField statusCodeTextField;
+	private JTextField resolutionCodeTextField;
+	
+	private JButton submitButton;
+	
+	private ClientUpdateTicketHandler clientUpdateTicketHandler;
+	
 	
 	public ClientTicketDialog(Frame owner, String clientUsername, String ticketID, HashMap<String, Ticket> activeTickets) {
 		super(owner, ticketID, false);
@@ -32,47 +40,69 @@ public class ClientTicketDialog extends JDialog {
 		this.ticketID = ticketID;
 		this.activeTickets = activeTickets;
 		
-		// TODO modify this code to accomodate displaying the ticket information
-		// May need to modify code to send along the entire ticket, rather than just the ticketID
-		// Or create a new class to handle transactions of tickets, to and from the server
+		// Create the TicketID label
+		ticketIDLabel = new JLabel("Ticket ID: " + ticketID);
+		
+		// Create the text areas where summary and resolution will be entered
+		summaryDescriptionTextArea = new JTextArea(60, 60);
+		resolutionDescriptionTextArea = new JTextArea(60, 60);
+		summaryDescriptionTextArea.setEditable(true);
+		resolutionDescriptionTextArea.setEditable(true);
+		
+		// Setup the two JScrollPanes for the ticket summary and resolution descriptions
+		JScrollPane summaryDescriptionScrollPane = new JScrollPane(summaryDescriptionTextArea);
+		JScrollPane resolutionDescriptionTextArea = new JScrollPane(resolutionDescriptionTextArea);
+		
+		JLabel summaryDescriptionLabel = new JLabel("Summary Description");
+		JLabel resolutionDescriptionLabel = new JLabel("Resolution Description");
+		
+		summaryDescriptionScrollPane.setColumnHeaderView(summaryDescriptionLabel);
+		resolutionDescriptionTextArea.setColumnHeaderView(resolutionDescriptionLabel);
 		
 		
-		textField = new JTextField(20);
-		textArea = new JTextArea();
-		textArea.setEditable(false);
+		// TODO change the statuscode and resolutioncode stuff to radio buttons
+		// put into the EAST border
+//		statusCodeTextField = new JTextField();
+//		resolutionCodeTextField = new JTextField();
 		
-		// Make panel to hold buttons
-		JPanel bottomPanel = new JPanel();
-		bottomPanel.add(textField);
+		
+		// Create the submit button
+		submitButton = new JButton("Submit");
+		
+		// Create the panels for the border layout
+		JPanel northPanel = new JPanel();
+		JPanel centerPanel = new JPanel();
+		JPanel southPanel = new JPanel();
+		
+		northPanel.add(ticketIDLabel);
+		southPanel.add(submitButton);
+		
+		centerPanel.setLayout(new FlowLayout());
+//		centerPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+		centerPanel.add(summaryDescriptionScrollPane);
+		centerPanel.add(resolutionDescriptionTextArea);
 		
 		// Get the content pane and add components
-		Container instantMessageDialogContentPane = this.getContentPane();
-		instantMessageDialogContentPane.add(bottomPanel, BorderLayout.SOUTH);
+		Container clientTicketDialogContentPane = this.getContentPane();
 		
-		JScrollPane messageScrollPane = new JScrollPane(textArea);
-		instantMessageDialogContentPane.add(messageScrollPane, BorderLayout.CENTER);
+		clientTicketDialogContentPane.add(northPanel, BorderLayout.NORTH);
+		clientTicketDialogContentPane.add(southPanel, BorderLayout.SOUTH);
+		clientTicketDialogContentPane.add(centerPanel, BorderLayout.CENTER);
 		
-		JLabel recipientMessageLabel = new JLabel("Sending message to " + recipient);
-		instantMessageDialogContentPane.add(recipientMessageLabel, BorderLayout.NORTH);
-		
+		// TODO may need to change the dimensions
 		this.setSize(400, 200);
 		
-		// Make a SendMessageHandler ActionListener and register the buttons and the JTextArea
-		sendMessageHandler = new SendMessageHandler(textField, textArea, recipient, this);
-		sendButton.addActionListener(sendMessageHandler);
-	}
-	
-	public void updateTextArea(String textToAppend) {
-		
-		// Add the sent text to the JTextArea
-		textArea.append(textToAppend + "\n");
-	}
-	
-	public void clearTextField() {
-		textField.setText("");
+		// Make a ClientUpdateTicketHandler ActionListener and register the submit button
+		// This class handles looking up the appropriate ticket, updating it and sending to the server
+		clientUpdateTicketHandler = new ClientUpdateTicketHandler(clientUsername, ticketID, activeTickets);
+		submitButton.addActionListener(clientUpdateTicketHandler);
 	}
 	
 	public String getClientUsername() {
 		return clientUsername;
+	}
+	
+	public String getTicketID() {
+		return ticketID;
 	}
 }
--- src/ClientUpdateTicketHandler.java
+++ src/ClientUpdateTicketHandler.java
@@ -4,3 +4,49 @@
 
 // This handler will be called from the ClientTicketDialog
 // The ClientTicketDialog will require ArrayList of active tickets
+
+/*
+	Listener class that handles sending messages from the client, via the InstantMessageDialog class
+	to the server.
+*/
+
+
+clientUpdateTicketHandler = new ClientUpdateTicketHandler(clientUsername, ticketID, activeTickets);
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ClientUpdateTicketHandler implements ActionListener {
+	
+	private String clientUsername;
+	private String ticketID;
+	private HashMap<String, Ticket> activeTickets;
+	
+	
+	public ClientUpdateTicketHandler(clientUsername, ticketID, activeTickets) {
+
+		this.clientUsername = clientUsername;
+		this.ticketID = ticketID;
+		this.activeTickets = activeTickets;
+	}
+	
+	public void actionPerformed(ActionEvent evt) {
+		// Get button clicked
+		JButton buttonClicked = (JButton) evt.getSource();
+		String actionCommand = new String(buttonClicked.getActionCommand());
+		
+		
+		textToSend = textField.getText();
+		
+		if (actionCommand.equals("Submit")) {
+			System.out.println(clientUsername + ": updating ticket...");
+			
+			// Recover ticket and update it
+			Ticket ticketToUpdate = (Ticket) activeTickets.get(ticketID);
+			
+			ticketToUpdate
+			
+		}
+	}
+}
